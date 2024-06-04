553c0b1bb907fe21da58d07f8cf85ea181247eac
YulinarNR <yulinarnurrahma@gmail.com>
2024-06-05 06:57:24 +0700

    react-router-component-tailwindcss-atomicdesign

---
+++ src/pages/login.jsx
@@ -0,0 +1,19 @@
+import AuthLayout from "../components/Layouts/AuthLayouts";
+import FormLogin from "../components/Fragments/FormLogin";
+import { Link } from "react-router-dom";
+const LoginPage = () => {
+  return (
+    <AuthLayout title="Login">
+      <FormLogin />
+      <p className="text-sm mt-5 text-center">
+        Don't have an account?{" "}
+        <Link to="/register" className="font-bold text-blue-600">
+          {" "}
+          Register
+        </Link>
+      </p>
+    </AuthLayout>
+  );
+};
+
+export default LoginPage;
